6abc444e7cc5f067f23625512ad362de3e3981b6
ksk1015 <ksk.aizawa@gmail.com>
2022-01-24 11:50:01 +0900

    canvasサイズの調整

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,34 @@ const lifeGame = new LifeGame(1, 1)
 const cellSize = 5
 const gutter = 1
 
+const useLifeGameSize = () => {
+  const viewportSize = useViewportSize()
+  const cols = Math.ceil((viewportSize.width - gutter) / (cellSize + gutter))
+  const rows = Math.ceil((viewportSize.height - gutter) / (cellSize + gutter))
+  const width = gutter + cols * (cellSize + gutter)
+  const height = gutter + rows * (cellSize + gutter)
+  return { cols, rows, width, height }
+}
+
 export function App() {
   console.log('App')
-  const { width, height } = useViewportSize()
+  const lifeGameSize = useLifeGameSize()
   useLayoutEffect(() => {
+    console.log('useLayoutEffect in App')
     const isInitial = lifeGame.length === 1
-    const cols = Math.floor((width - gutter) / (cellSize + gutter))
-    const rows = Math.floor((height - gutter) / (cellSize + gutter))
-    lifeGame.setSize(cols, rows)
+    lifeGame.setSize(lifeGameSize.cols, lifeGameSize.rows)
     if (isInitial) {
       lifeGame.random()
       lifeGame.play()
     }
-  }, [width, height])
+  }, [lifeGameSize])
   return (
     <div style="position: absolute; left: 0; top: 0; width: 100%; height: 100%">
       <LifeGameCanvas
         lifeGame={lifeGame}
         cellSize={cellSize}
         gutter={gutter}
-        style="width: 100%; height: 100%"
+        style={`width: ${lifeGameSize.width}px; height: ${lifeGameSize.height}px`}
       />
       <div style="position: absolute; bottom: 5px; right: 5px">
         <button onClick={() => lifeGame.togglePlay()}>Play/Pause</button>
